d7e0bb1648248afa2e87cf5510b404e899f8e958
Mostafa-ElHawary <alhwarymostafa@gmail.com>
2023-11-05 21:27:22 +0200

    edit

--- operations at c++/Linked-list.cpp
+++ operations at c++/Linked-list.cpp
@@ -40,15 +40,15 @@ public:
         if (isEmpty())
         {
             head = newNode;
-            tail = newNode;
-            len++;
+            tail = newNode; 
+           
         }
         else
-        {
+        { 
             tail->next = newNode;
             tail = newNode;
-            len++;
         }
+            len++;
     };
 
     void InsertFront(int val)
@@ -67,6 +67,58 @@ public:
         len++;
     }
 
+   // we need to check if the indx is valid ot is't tail
+  // if the indx == len - 1 insert at end .. okay 
+
+//  now if the insertion in the midle of linked list 
+        // try to insert a val by indx
+        // creat a curr ptr to traverse the list
+        // like example we will insert it at indx 2  
+        // if indx is 2 then we will insert it at indx 2
+        // let's check if this is true 
+       /// i'm thinking ...
+      // out of scope !! 
+    void InsertByIndx(int val, int indx ){
+      // if the indx < 0 or > len
+      if(indx < 0 || indx > len){
+        cout << "Vailid Indx"<< endl ; 
+        return; 
+      }
+      Node* newNode = new Node(val);
+      
+      // if the insertion to front 
+      //mean indx == 0
+      if(indx == 0  ){
+        newNode->next = head ;
+        head = newNode;
+      }
+      // if the insertion to end
+       if(indx == len){
+         tail->next = newNode;
+         tail = newNode;
+       }
+      
+        //len iT'S mean the length
+      // if the indx < 0 or > len
+
+      // if the indx ther in the midle in the linked list
+      else {
+        Node* curr = head; // ptr for traversing the list
+        for(int i= 0; i < indx - 1;++i){
+
+           curr = curr->next;
+          
+        } // we can use while loop
+        newNode->next = curr->next;
+        // if the indx is 2 then we will insert it at indx 2
+        // so here 
+        curr->next = newNode ;
+      }
+      len++;
+      //let's try this solution
+
+    };
+
     void Print()
     {
         for (Node *cur = head; cur; cur = cur->next)
@@ -75,6 +127,7 @@ public:
         }
         cout << endl;
     }
+
 
     void getItemByIndex(int indx)
     {
@@ -114,31 +167,70 @@ public:
     }
 };
 
+
+
 int main()
 {
 
-    Linked list;
-    // list.Insert(100);
-    int CountItems;
+   Linked list;
+
+   int CountItems ;
     cout << "Inter count of items you need added : ";
     cin >> CountItems;
-    for (int i = 0; i < CountItems; i++)
-    {
-        cout << "Inter  items " << i << " : ";
-        int val;
-        cin >> val;
-        list.Insert(val);
+    for(int i = 0 ; i < CountItems ; i++){
+      cout << "Inter  items " << i << " : ";
+      int val;
+      cin >> val;
+      list.InsertEnd(val);
     }
     list.Len();
     list.Print();
-    list.getItemByIndex(3);
-    list.Search(50);
-    int InsertItems ;
-    cout << "Inter the item for intsertion : ";
-    cin >> InsertItems;
-    list.InsertFront(InsertItems);
-    list.Len();
+
+
+    //  int size;
+  //   int data;
+  //   int countOfElements;
+
+  //   std::cout << "Enter the size of the array: ";
+  //   std::cin >> size;
+  //   Array Arr(size);
+
+  //   std::cout << "How many elements do you want to enter in this array: ";
+  //   std::cin >> countOfElements;
+
+  //   for (int i = 0; i < countOfElements; i++) {
+  //       std::cout << "Enter the element: ";
+  //       std::cin >> data;
+  //       Arr.push(data);
+  //   }
+
+  //   std::cout << "Length after pushing: " << Arr.Len() << std::endl;
+  //   std::cout << "Size after pushing: " << Arr.Size() << std::endl;
+
+  //   std::cout << "Elements in the array: ";
+  //   Arr.Display();
+
+  //   // Insert an element at a specific index
+  //   int valueToInsert, indexToInsert;
+  //   std::cout << "\nEnter a value to insert: ";
+  //   std::cin >> valueToInsert;
+  //   std::cout << "Enter the index where you want to insert: ";
+  //   std::cin >> indexToInsert;
+
+  //   Arr.Insert(valueToInsert, indexToInsert);
+
+  //   std::cout << "Updated array after insertion: ";
+  //   Arr.Display();
+
+  // // Remove an element from the array
+  // int valueToRemove;
+  // std::cout << "\nEnter a value to remove: ";
+  //   std::cin >> valueToRemove;
+  // Arr.RemoveVal(valueToRemove);
+  // std::cout << "Updated array after removal: ";
+  // Arr.Display();
   
-    list.Print();
+
+
     return 0;
-}
+};
